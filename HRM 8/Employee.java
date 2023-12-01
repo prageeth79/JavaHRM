52a85d059508102943e794ef4600026e230b26a1
prageeth79 <prageeth79@gmail.com>
2023-12-01 20:24:06 +0530

    Add files via upload

---
+++ HRM 8/Employee.java
@@ -0,0 +1,140 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class Employee extends DB{
+
+    public String code;
+    public String fullName;
+    public String nameWithInit;
+    public String nic;
+    public String dateOfBirth;
+    public String gender;
+    public Designation designation = new Designation();
+    public Department department = new Department();
+    public Dictionary<String,Integer> fields = new Hashtable<>();
+    public Employee(){
+        fields.put("code",1);
+        fields.put("fullName",2);
+        fields.put("NameWIthInit",3);
+        fields.put("NIC",4);
+        fields.put("dofb",5);
+        fields.put("Gender",6);
+        fields.put("Designation",7);
+        fields.put("Department",8);
+    }
+
+    public boolean matchCode(){
+        return code.matches("/^[0-9]*$/");
+    }
+
+    public  boolean matchFullName(){
+        return fullName.matches("/^[a-zA-Z ]*$/");
+    }
+
+    public  boolean matchNameWithInit(){
+        return fullName.matches("/^[a-zA-Z .]*$/");
+    }
+
+    public  boolean matchNic(){
+        return nic.matches("/^\\d{9}[VX]$/") || nic.matches("/^\\d{12}$");
+    }
+
+    public boolean matchDateOfBirth(){
+        return dateOfBirth.matches("/\\d{4}-\\d{2}-\\d{2}");
+    }
+
+    public boolean validate(){
+        return matchCode() && matchFullName() && matchNameWithInit() && matchNic() && matchDateOfBirth();
+    }
+
+    public boolean save(){
+        return save(code,fullName,nameWithInit,nic,dateOfBirth,gender,designation.code,department.code);
+    }
+
+    public  boolean update(){
+        return update(code,fullName,nameWithInit,nic,dateOfBirth,gender,designation.code,department.code);
+    }
+
+    public boolean delete(){
+        return delete(code);
+    }
+
+    public boolean save(String code, String fullName, String nameWithInit, String nic, String DofB, String gender, String desig, String dept){
+        String sql = "Insert into employee Values('" + code + "','" + fullName + "','" + nameWithInit + "','" + nic + "','" + DofB + "','" + gender + "','" + desig +"','" + dept + "')";
+        return super.save(sql);
+    }
+    public boolean update(String code, String fullName, String nameWithInit, String nic, String DofB, String gender, String desig, String dept){
+        String sql = "Update employee SET fullName = '" + fullName + "',nameWithInit = '" + nameWithInit + "', NIC = '" + nic + "', dofb = '" + DofB + "', Gender = '" + gender + "', Designation = '" + dept +"', Department = '" + desig + "' WHERE code = '" + code + "'";
+        return super.update(sql);
+    }
+
+    @Override
+    public boolean delete(String code){
+        String sql = "Delete from employee where code ='" + code + "'";
+        return  super.delete(sql);
+    }
+
+    public String[] load(String code){
+        String sql = "SELECT * from employee WHERE code = '" + code + "'";
+        //Connection con = dbConnect();
+        ResultSet rs = dbResult(con, sql);
+        String[] data = new String[8];
+        try {
+            rs.next();
+            data[0] = rs.getString(1);
+            data[1] = rs.getString(2);
+            data[2] = rs.getString(3);
+            data[3] = rs.getString(4);
+            data[4] = rs.getString(5);
+            data[5] = rs.getString(6);
+            data[6] = rs.getString(7);
+            data[7] = rs.getString(8);
+
+            this.code = data[0];
+            this.fullName = data[1];
+            this.nameWithInit = data[2];
+            this.nic = data[3];
+            this.dateOfBirth = data[4];
+            this.gender = data[5];
+            this.designation.load(data[6]);
+            this.department.load(data[7]);
+            return data;
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return  null;
+    }
+
+    public String[][] employeeDetailsAll() {
+        try{
+            //Connection con = this.dbConnect();
+            ResultSet rs= this.dbResult(con, "select count(*) from Employee");
+            rs.next();
+            int rows = rs.getInt(1);
+            String[][] data = new String[rows][8];
+            rs = this.dbResult(con, "select e.*,d.depName,de.desigName from Employee e,Department D, Designation De where e.Designation = De.code and e.Department = D.code order by code");
+            int i = 0;
+            while(rs.next()) {
+                data[i][0] = rs.getString(1);
+                data[i][1] = rs.getString(2);
+                data[i][2] = rs.getString(3);
+                data[i][3] = rs.getString(4);
+                data[i][4] = rs.getString(5);
+                data[i][5] = rs.getString(6);
+                data[i][6] = rs.getString(9);
+                data[i][7] = rs.getString(10);
+                i++;
+                //System.out.println(rs.getString(1)+"  "+rs.getString(2));
+            }
+            //con.close();
+            return data;
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return new String[1][8];
+    }
+}
---
+++ HRM 8/PersonalInfo.java
@@ -0,0 +1,378 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PersonalInfo extends JFrame implements ActionListener {
+    String sDesignationId, sDepartmentId;
+    JLabel lblEmployeeNo, lblEmployeeFullName, lblEmployeeNameWithInit, lblEmployeeNIC, lblEmployeeGender,lblEmployeeDOFB, lblEmployeeDesignation, lblEmployeeDepartment,lblEmpSearch;
+    JTextField txtEmployeeNo, txtEmployeeFullName, txtEmployeeNameWithInit, txtEmployeeNIC, txtEmployeeDOFB, txtEmployeeDesignation, txtEmployeeDepartment,txtEmpSearch;
+    JComboBox cmbEmployeeGender,cmbEmpSearchField;
+    JTable tblEmployeeDesignation,tblEmployeeDepartment,tblEmployee;
+    JLabel lblTblEmpDesignation,lblTblEmpDepartment;
+    JScrollPane tblEmpDesigPane,tblEmpDepartPane,tblEmployeePane;
+    JButton btnNew, btnSave, btnUpdate, btnDelete, btnCancel,btnAddDesig, btnAddDepart;
+    Font fntLblFont,fntTxtFont,fntBtnFont;
+    Employee db;
+
+    String[] designationFieldNames = {"Code", "Designation"};
+    String[] departmentFieldNames = {"Code", "Department", "Tel", "Manager"};
+    String[] employeeFieldNames = {"Code", "Full Name", "Name With Init","NIC","Date of Birth","Gender","Department","Designation"};
+
+    public PersonalInfo(){
+        db = new Employee();
+        this.setWindow();
+        this.setComponents(db);
+        this.gridSelection();
+        this.adjestTableColumn();
+        this.addComponents();
+        this.setVisible(true);
+    }
+
+    private  void setComponents(Employee db){
+
+        fntLblFont = new Font("Comic Sans",Font.BOLD, 16);
+        fntTxtFont = new Font("Comic Sans",Font.PLAIN, 14);
+        fntBtnFont = new Font("Comic Sans",Font.BOLD, 14);
+
+        lblEmployeeNo = new JLabel("Employee No");
+        lblEmployeeNo.setBounds(10,10, 150, 25);
+        lblEmployeeNo.setFont(fntLblFont);
+
+        txtEmployeeNo = new JTextField();
+        txtEmployeeNo.setBounds(200, 10, 100, 25);
+        txtEmployeeNo.setFont(fntTxtFont);
+
+        lblEmployeeFullName = new JLabel("Employee Full Name");
+        lblEmployeeFullName.setBounds(10, 40, 250, 25);
+        lblEmployeeFullName.setFont(fntLblFont);
+
+        txtEmployeeFullName = new JTextField();
+        txtEmployeeFullName.setBounds(200, 40, 250, 25);
+        txtEmployeeFullName.setFont(fntTxtFont);
+
+        lblEmployeeNameWithInit = new JLabel("Name with Initials");
+        lblEmployeeNameWithInit.setBounds(10, 70, 150, 25);
+        lblEmployeeNameWithInit.setFont(fntLblFont);
+
+        txtEmployeeNameWithInit = new JTextField();
+        txtEmployeeNameWithInit.setBounds(200, 70, 200, 25);
+        txtEmployeeNameWithInit.setFont(fntTxtFont);
+
+        lblEmployeeNIC = new JLabel("National ID Number");
+        lblEmployeeNIC.setBounds(10, 100, 150, 25);
+        lblEmployeeNIC.setFont(fntLblFont);
+
+        txtEmployeeNIC = new JTextField();
+        txtEmployeeNIC.setBounds(200, 100, 150, 25);
+        txtEmployeeNIC.setFont(fntTxtFont);
+
+        lblEmployeeGender = new JLabel("Gender");
+        lblEmployeeGender.setBounds(10, 130, 150, 25);
+        lblEmployeeGender.setFont(fntLblFont);
+
+        String gender[] = {"male", "female"};
+        cmbEmployeeGender = new JComboBox(gender);
+        cmbEmployeeGender.setBounds(200, 130, 100, 25);
+        cmbEmployeeGender.setFont(fntTxtFont);
+
+        lblEmployeeDOFB = new JLabel("Date of Birth");
+        lblEmployeeDOFB.setBounds(10, 160, 150, 25);
+        lblEmployeeDOFB.setFont(fntLblFont);
+
+        txtEmployeeDOFB = new JTextField();
+        txtEmployeeDOFB.setBounds(200, 160, 150, 25);
+        txtEmployeeDOFB.setFont(fntTxtFont);
+
+        lblEmployeeDesignation = new JLabel("Designation");
+        lblEmployeeDesignation.setBounds(10, 190, 150, 25);
+        lblEmployeeDesignation.setFont(fntLblFont);
+
+        txtEmployeeDesignation = new JTextField();
+        txtEmployeeDesignation.setBounds(200, 190, 250, 25);
+        txtEmployeeDesignation.setFont(fntTxtFont);
+        txtEmployeeDesignation.setEditable(false);
+
+        btnAddDesig = new JButton("+");
+        btnAddDesig.setBounds(460,190,25,25);
+        btnAddDesig.setFont(fntLblFont);
+
+        lblEmployeeDepartment = new JLabel("Department");
+        lblEmployeeDepartment.setBounds(10, 220, 150, 25);
+        lblEmployeeDepartment.setFont(fntLblFont);
+
+        txtEmployeeDepartment = new JTextField();
+        txtEmployeeDepartment.setBounds(200, 220, 250, 25);
+        txtEmployeeDepartment.setFont(fntTxtFont);
+        txtEmployeeDepartment.setEditable(false);
+
+        btnAddDepart = new JButton("+");
+        btnAddDepart.setBounds(460,220,25,25);
+        btnAddDepart.setFont(fntLblFont);
+
+        lblTblEmpDesignation = new JLabel("Designation");
+        lblTblEmpDesignation.setBounds(500, 10,300,20);
+        String[][] data = new String[1][2];
+        data = db.designation.designationDetailsAll();
+
+        tblEmployeeDesignation = new JTable(data, designationFieldNames);
+        //tblEmployeeDesignation.setBounds(500, 10, 300, 100);
+        tblEmployeeDesignation.setFont(fntTxtFont);
+        tblEmpDesigPane = new JScrollPane(tblEmployeeDesignation);
+        tblEmpDesigPane.setBounds(500, 25, 300, 100);
+        tblEmpDesigPane.setVisible(true);
+
+        lblTblEmpDepartment = new JLabel("Department");
+        lblTblEmpDepartment.setBounds(500, 130,300,20);
+
+        data = db.department.departmentDetailsAll();
+
+        tblEmployeeDepartment = new JTable(data, departmentFieldNames);
+        //tblEmployeeDesignation.setBounds(500, 10, 300, 100);
+        tblEmployeeDepartment.setFont(fntTxtFont);
+        tblEmpDepartPane = new JScrollPane(tblEmployeeDepartment);
+        tblEmpDepartPane.setBounds(500, 150, 300, 100);
+        tblEmpDepartPane.setVisible(true);
+
+        lblEmpSearch = new JLabel("Search Employee");
+        lblEmpSearch.setBounds(370, 310, 130, 25);
+        lblEmpSearch.setFont(fntTxtFont);
+
+        txtEmpSearch = new JTextField();
+        txtEmpSearch.setBounds(490,310,200,25);
+        txtEmpSearch.setFont(fntTxtFont);
+
+        String fields[] = {"Id", "Full Name","NIC", "Department", "Designation"};
+        cmbEmpSearchField = new JComboBox(fields);
+        cmbEmpSearchField.setBounds(700, 310, 100, 25);
+        cmbEmpSearchField.setFont(fntTxtFont);
+
+        data = db.employeeDetailsAll();
+
+        tblEmployee = new JTable(data, employeeFieldNames);
+        //tblEmployeeDesignation.setBounds(500, 10, 300, 100);
+        tblEmployee.setFont(fntTxtFont);
+        tblEmployeePane = new JScrollPane(tblEmployee);
+        tblEmployeePane.setBounds(10, 350, 800, 250);
+        tblEmployeePane.setVisible(true);
+
+        ImageIcon inew = new ImageIcon("new.png");
+        btnNew = new JButton("NEW");
+        btnNew.setBounds(10, 260, 120, 40);
+        btnNew.setFont(fntBtnFont);
+        btnNew.setIcon(inew);
+        btnNew.addActionListener(this);
+
+        ImageIcon isave = new ImageIcon("save.png");
+        btnSave = new JButton("SAVE");
+        btnSave.setBounds(150, 260, 120, 40);
+        btnSave.setFont(fntBtnFont);
+        btnSave.setIcon(isave);
+        btnSave.addActionListener(this);
+
+        ImageIcon iupdate = new ImageIcon("update.png");
+        btnUpdate = new JButton("UPDAGE");
+        btnUpdate.setBounds(290, 260, 120, 40);
+        btnUpdate.setFont(fntBtnFont);
+        btnUpdate.setIcon(iupdate);
+        btnUpdate.addActionListener(this);
+
+        ImageIcon idelete = new ImageIcon("delete.png");
+        btnDelete = new JButton("DELETE");
+        btnDelete.setBounds(430, 260, 120, 40);
+        btnDelete.setFont(fntBtnFont);
+        btnDelete.setIcon(idelete);
+        btnDelete.addActionListener(this);
+
+        ImageIcon icancel = new ImageIcon("close.png");
+        btnCancel = new JButton("CANCEL");
+        btnCancel.setBounds(680, 260, 120, 40);
+        btnCancel.setFont(fntBtnFont);
+        btnCancel.setIcon(icancel);
+    }
+
+    private  void adjestTableColumn(){
+        tblEmployeeDesignation.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        tblEmployeeDesignation.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tblEmployeeDesignation.getColumnModel().getColumn(1).setPreferredWidth(200);
+
+        tblEmployeeDepartment.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        tblEmployeeDepartment.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tblEmployeeDepartment.getColumnModel().getColumn(1).setPreferredWidth(90);
+        tblEmployeeDepartment.getColumnModel().getColumn(2).setPreferredWidth(80);
+        tblEmployeeDepartment.getColumnModel().getColumn(3).setPreferredWidth(100);
+    }
+
+    private void addComponents(){
+        this.add(lblEmpSearch);
+        this.add(txtEmpSearch);
+        this.add(cmbEmpSearchField);
+
+        this.add(tblEmployeePane);
+        this.add(lblTblEmpDesignation);
+        this.add(tblEmpDesigPane);
+
+        this.add(btnNew);
+        this.add(btnSave);
+        this.add(btnUpdate);
+        this.add(btnDelete);
+        this.add(btnCancel);
+        this.add(btnAddDepart);
+        this.add(btnAddDesig);
+
+        this.add(tblEmpDepartPane);
+        this.add(lblTblEmpDepartment);
+        this.add(txtEmployeeDepartment);
+        this.add(lblEmployeeDepartment);
+        this.add(txtEmployeeDesignation);
+        this.add(lblEmployeeDesignation);
+        this.add(txtEmployeeDOFB);
+        this.add(lblEmployeeDOFB);
+        this.add(cmbEmployeeGender);
+        this.add(lblEmployeeGender);
+        this.add(txtEmployeeNIC);
+        this.add(lblEmployeeNIC);
+        this.add(txtEmployeeNameWithInit);
+        this.add(lblEmployeeNameWithInit);
+        this.add(txtEmployeeFullName);
+        this.add(lblEmployeeFullName);
+        this.add(txtEmployeeNo);
+        this.add(lblEmployeeNo);
+    }
+
+    private void gridSelection(){
+        ListSelectionModel tblEmpDeptMod = tblEmployeeDepartment.getSelectionModel();
+        tblEmpDeptMod.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tblEmpDeptMod.addListSelectionListener(new ListSelectionListener(){
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                sDepartmentId = tblEmployeeDepartment.getValueAt(tblEmployeeDepartment.getSelectedRow(), 0).toString();
+                txtEmployeeDepartment.setText(tblEmployeeDepartment.getValueAt(tblEmployeeDepartment.getSelectedRow(), 1).toString());
+            }
+        });
+
+        ListSelectionModel tblEmpDesigMod = tblEmployeeDesignation.getSelectionModel();
+        tblEmpDesigMod.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tblEmpDesigMod.addListSelectionListener(new ListSelectionListener(){
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                sDesignationId = tblEmployeeDesignation.getValueAt(tblEmployeeDesignation.getSelectedRow(), 0).toString();
+                txtEmployeeDesignation.setText(tblEmployeeDesignation.getValueAt(tblEmployeeDesignation.getSelectedRow(), 1).toString());
+            }
+        });
+
+        ListSelectionModel tblEmpMod = tblEmployee.getSelectionModel();
+        tblEmpMod.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tblEmpMod.addListSelectionListener(new ListSelectionListener(){
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+               String sEmpCode = tblEmployee.getValueAt(tblEmployee.getSelectedRow(), 0).toString();
+               db.load(sEmpCode);
+               loadTextbox();
+
+            }
+        });
+    }
+
+    public void loadTextbox(){
+        txtEmployeeNo.setText(db.code);
+        txtEmployeeFullName.setText(db.fullName);
+        txtEmployeeNameWithInit.setText(db.nameWithInit);
+        txtEmployeeNIC.setText(db.nic);
+        txtEmployeeDOFB.setText(db.dateOfBirth);
+        cmbEmployeeGender.setSelectedItem(db.gender);
+        sDesignationId = db.designation.code;
+        txtEmployeeDesignation.setText(db.designation.designation);
+        sDepartmentId = db.department.code;
+        txtEmployeeDepartment.setText(db.department.departmentName);
+    }
+
+    private void setWindow(){
+        this.setTitle("Personal Inforamtion");
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(850,650);
+        this.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getSize().width) / 2,
+                (Toolkit.getDefaultToolkit().getScreenSize().height - this.getSize().height) / 2);
+
+    }
+
+    public void clearFields(){
+        txtEmployeeNo.setText("");
+        txtEmployeeFullName.setText("");
+        txtEmployeeNameWithInit.setText("");
+        txtEmployeeNIC.setText("");
+        txtEmployeeDOFB.setText("");
+        txtEmployeeDesignation.setText("");
+        txtEmployeeDepartment.setText("");
+        sDesignationId = "";
+        sDepartmentId = "";
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == btnNew){
+            clearFields();
+            //System.out.println(cmbEmployeeGender.getSelectedItem().toString());
+        }
+        if(e.getSource() == btnSave){
+            if(!db.validate()) {
+                JOptionPane.showMessageDialog(this,"Data validation Fail",
+                        "Validator",JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            if(db.save(txtEmployeeNo.getText(),txtEmployeeFullName.getText(),txtEmployeeNameWithInit.getText(),
+                    txtEmployeeNIC.getText(),txtEmployeeDOFB.getText(),cmbEmployeeGender.getSelectedItem().toString(),
+                    sDesignationId, sDepartmentId)) {
+                tblEmployee.setModel(new DefaultTableModel(db.employeeDetailsAll(), employeeFieldNames));
+                clearFields();
+                JOptionPane.showMessageDialog(this,
+                        "Record Added Successfully.", "ADD RECORD", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(this,
+                        "Error In Record Adding", "ADD RECORD", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        if(e.getSource() == btnUpdate){
+            if(!db.validate()) {
+                JOptionPane.showMessageDialog(this,"Data validation Fail",
+                        "Validator",JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            if(db.update(txtEmployeeNo.getText(),txtEmployeeFullName.getText(),txtEmployeeNameWithInit.getText(),
+                    txtEmployeeNIC.getText(),txtEmployeeDOFB.getText(),cmbEmployeeGender.getSelectedItem().toString(),
+                    sDesignationId,sDepartmentId)) {
+                tblEmployee.clearSelection();
+                tblEmployee.setModel(new DefaultTableModel(db.employeeDetailsAll(), employeeFieldNames));
+                clearFields();
+                JOptionPane.showMessageDialog(this,
+                        "Record Updated Successfully.", "ADD RECORD", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(this,
+                        "Error In Record Update", "ADD RECORD", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
+        if(e.getSource() == btnDelete){
+            if(db.delete(txtEmployeeNo.getText())) {
+                tblEmployee.clearSelection();
+                tblEmployee.setModel(new DefaultTableModel(db.employeeDetailsAll(), employeeFieldNames));
+                clearFields();
+                JOptionPane.showMessageDialog(this,
+                        "Record Deleted Successfully.", "ADD RECORD", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(this,
+                        "Error In Record Delete", "ADD RECORD", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
+//        if(e.getSource() == tblEmployeeDesignation) {
+//            sDesignationId = tblEmployeeDesignation.getValueAt(tblEmployeeDesignation.getSelectedRow(), 0).toString();
+//            txtEmployeeDesignation.setText(tblEmployeeDesignation.getValueAt(tblEmployeeDesignation.getSelectedRow(), 1).toString());
+//        }
+    }
+}
